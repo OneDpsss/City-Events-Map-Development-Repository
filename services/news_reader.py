688b1535fe1b8a83b86bb8829917403d0179a7ff
Vladimir Bezrukov <82569672+Protogenic@users.noreply.github.com>
2024-04-29 20:35:10 +0300

    Add files via upload

--- services/news_reader.py
+++ services/news_reader.py
@@ -7,6 +7,7 @@ from io import BytesIO
 
 import json
 from datetime import datetime
+import time
 from bs4 import BeautifulSoup
 from internal.function import response_to_server, filter_func, SummarizeAiFunc
 
@@ -14,7 +15,7 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_config.settings")
 
-news_limit = 3
+news_limit = 1
 
 
 def get_after_find(article, par1, par2):
@@ -47,7 +48,7 @@ def download_image(url, save_path):
         return None
 
 
-# add existence checker and delete news after 7 days
+#add existence checker and delete news after 7 days
 
 async def send_json(img, post, url):
     try:
@@ -66,68 +67,71 @@ async def send_json(img, post, url):
     data['url'] = url
     data['img'] = img
     data['news_date'] = time
-    # response_to_server(data)
-    print(data['title'])
+    response_to_server(data)
 
 
 async def nnru():
-    site_url = 'https://www.nn.ru/text/'
-    data = check_response(site_url)
-    if data is None:
-        return
+    prev_data = None
+    while True:
+        site_url = 'https://www.nn.ru/text/'
+        data = check_response(site_url)
+        if not (data is None or prev_data == data):
+            for article in data.find_all('article', class_='OPHIx')[:news_limit]:
 
-    for article in data.find_all('article', class_='OPHIx')[:news_limit]:
+                rubrics = article.find_all(attrs={"slot": "rubrics"})
+                city_news = False
+                for r in rubrics:
+                    if r['title'] == 'Город' or r['title'] == 'Дороги и транспорт':
+                        city_news = True
 
-        rubrics = article.find_all(attrs={"slot": "rubrics"})
-        city_news = False
-        for r in rubrics:
-            if r['title'] == 'Город' or r['title'] == 'Дороги и транспорт':
-                city_news = True
+                if not city_news:
+                    continue
 
-        if not city_news:
-            continue
+                title = None
+                tag = article.find('h2')
+                if tag:
+                    title = tag.a['title']
 
-        title = None
-        tag = article.find('h2')
-        if tag:
-            title = tag.a['title']
+                url = 'https://www.nn.ru/' + article.a['href']
+                img = get_after_find(article, 'img', 'src')
 
-        url = 'https://www.nn.ru/' + article.a['href']
-        img = get_after_find(article, 'img', 'src')
+                response = requests.get(url)
+                news_data = BeautifulSoup(response.text, 'html.parser')
+                text = title
+                if news_data.find('div', {'class': 'qQq9J'}) is not None:
+                    text += ". " + news_data.find('div', {'class': 'qQq9J'}).get_text(strip=True)
+                if text == "":
+                    return
 
-        response = requests.get(url)
-        news_data = BeautifulSoup(response.text, 'html.parser')
-        text = title
-        if news_data.find('div', {'class': 'qQq9J'}) is not None:
-            text += ". " + news_data.find('div', {'class': 'qQq9J'}).get_text(strip=True)
-        if text == "":
-            return
-
-        post = await SummarizeAiFunc(text)
-        await send_json(img, post, url)
+                post = await SummarizeAiFunc(text)
+                await send_json(img, post, url)
+                prev_data = data
+                time.sleep(600)
 
 
 async def rbc():
-    site_url = 'https://nn.rbc.ru/nn/'
-    data = check_response(site_url)
-    if data is None:
-        return
+    prev_data = None
+    while True:
+        site_url = 'https://nn.rbc.ru/nn/'
+        data = check_response(site_url)
+        if not (data is None or prev_data == data):
+            for article in data.find_all('div', {'class': 'item js-rm-central-column-item item_image-mob js-category-item'})[
+                           :news_limit]:
 
-    for article in data.find_all('div', {'class': 'item js-rm-central-column-item item_image-mob js-category-item'})[
-                   :news_limit]:
-        title = article.find('span', {'class': 'normal-wrap'}).text
-        url = article.find('a', {'class': 'item__link rm-cm-item-link js-rm-central-column-item-link'}).get('href')
-        img = get_after_find(article, 'img', 'src')
+                url = article.find('a', {'class': 'item__link rm-cm-item-link js-rm-central-column-item-link'}).get('href')
+                img = get_after_find(article, 'img', 'src')
 
-        response = requests.get(url)
-        news_data = BeautifulSoup(response.text, 'html.parser')
-        text = ""
-        if news_data.find('div', {'class': 'article__text__overview'}) is not None:
-            text = news_data.find('div', {'class': 'article__text__overview'}).get_text(strip=True)
-        if text == "":
-            return
-        post = await SummarizeAiFunc(text)
-        await send_json(img, post, url)
+                response = requests.get(url)
+                news_data = BeautifulSoup(response.text, 'html.parser')
+                text = ""
+                if news_data.find('div', {'class': 'article__text__overview'}) is not None:
+                    text = news_data.find('div', {'class': 'article__text__overview'}).get_text(strip=True)
+                if text == "":
+                    return
+                post = await SummarizeAiFunc(text)
+                await send_json(img, post, url)
+                prev_data = data
+                time.sleep(600)
 
 
 def run():
