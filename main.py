2f439adaddf4b87c5e36d3e3abe7d2f9a3f0c3bd
Valeriy <108849165+OneDpsss@users.noreply.github.com>
2024-05-02 14:38:50 +0300

    commit

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+# Импорт необходимых модулей
+import asyncio
+import logging
+import datetime
 from config.config import api_id, api_hash
 from config.sources.sources import tg_channels
 from services.tg_grabber import telegram_grabber
-from services.news_reader import nnru, rbc
-import asyncio
+from services.news_reader import nnru
 
-session = "myGrab"
-amount_messages = 10
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
+# Настройка логгера
+logging.basicConfig(level=logging.INFO, filename="py_log.log",filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger("myGrab")
 
 
+# Определение основной асинхронной функции
 async def main():
-    await asyncio.gather(nnru(), telegram_grabber(session, api_id, api_hash, loop=loop, tg_channels=tg_channels))
+    # Название сессии для Telegram
+    session = "myGrab"
+
+    # Создание нового цикла событий
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    # Собираем задачи для одновременного выполнения
+    await asyncio.gather(
+        #nnru()
+        # nnru(),  # Загрузка новостей с nn.ru
+        telegram_grabber(session, api_id, api_hash, logger, loop=loop, tg_channels=tg_channels,)
+        # Загрузка сообщений из каналов Telegram
+
+    )
 
 
+
+# Запуск основной функции
+logger.info(f"Запуск основных задач.Время:{datetime.datetime.now()}")
 asyncio.run(main())
--- services/tg_grabber.py
+++ services/tg_grabber.py
@@ -5,35 +5,52 @@ from telethon import TelegramClient, events
 from internal.function import response_to_server, filter_func, SummarizeAiFunc
 
 
-async def telegram_grabber(session, api_id, api_hash, loop=None, tg_channels=None):
+async def telegram_grabber(session, api_id, api_hash,logger, loop=None, tg_channels=None,):
+    # Инициализация клиента Telegram
     client = TelegramClient(session, api_id, api_hash, system_version="4.16.30-vxCUSTOM", loop=loop)
     await client.start()
 
+    # Обработчик новых сообщений
     @client.on(events.NewMessage(chats=tg_channels))
     async def check_events(event):
         new_text = event.raw_text.replace("\n", "")
-        if not (filter_func(new_text)):
-            print("Не прошло")
-            return client
+        if not filter_func(new_text):
+            print("Фильтрация не пройдена")
+            return
+
+        # Создание ссылки на сообщение
         message_link = f"https://t.me/{event.chat.username}/{event.id}"
+
+        # Суммаризация текста сообщения
         post = await SummarizeAiFunc(new_text)
-        if post == "":
-            return client
+        if not post:
+            return
+
+        # Обработка полученной суммаризации
         data = json.loads(post)
         data['url'] = message_link
         data['img'] = None
+
+        # Установка даты публикации
         time = datetime.now().strftime("%Y-%m-%d")
         data['news_date'] = time
-        time = datetime.now().strftime("%d-%m-%Y%H-%M-%S-%f")
-        path = f"images/img+{time}"
-        if event.message.photo:
-            await event.download_media(path)
-        with open(path+".jpg", "rb") as file:
-            image_data = file.read()
-            img = base64.b64encode(image_data).decode('utf-8')
-        data['img'] = img
-        print(data)
-        #response_to_server(data)
-        ##send_message_func(data)
 
+        # Обработка изображения, если оно есть
+        if event.message.photo:
+            time = datetime.now().strftime("%d-%m-%Y%H-%M-%S-%f")
+            path = f"images/img+{time}"
+            await event.download_media(path)
+            with open(path + ".jpg", "rb") as file:
+                image_data = file.read()
+                img = base64.b64encode(image_data).decode('utf-8')
+            data['img'] = img
+        if data["priority"] == 0:
+            return
+        # Отправка данных на сервер
+        logger.info(f"{json.dumps(data["url"])}")
+        print(data)
+        # response_to_server(data)
+        # send_message_func(data)
+
+    # Запуск клиента Telegram
     await client.run_until_disconnected()
