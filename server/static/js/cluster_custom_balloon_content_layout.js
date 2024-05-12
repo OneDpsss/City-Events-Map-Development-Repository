79fee3d9ff8388c4ea43eea4c2a98dde347bfaa9
codlxflare <148866504+codlxflare@users.noreply.github.com>
2024-05-12 15:35:55 +0300

    Update2 cluster_custom_balloon_content_layout.js

--- server/static/js/cluster_custom_balloon_content_layout.js
+++ server/static/js/cluster_custom_balloon_content_layout.js
@@ -1,30 +1,31 @@
-
 ymaps.ready(function () {
        var myMap = new ymaps.Map('map', {
   center: [56.326797, 44.006516],
   zoom: 9,
-  controls: ['zoomControl'],
-  behaviors: ['drag'],
+  
+  
   searchControlProvider: 'yandex#search'
 });   
    
    myMap.options.set('minZoom', 5);
-   myMap.options.set('maxZoom', 14);
+   
    
 
 
     var clusterer = new ymaps.Clusterer({
-    preset: 'islands#invertedPinkClusterIcons',
-    clusterDisableClickZoom: true,
-    openBalloonOnClick: false  
+    preset: 'islands#invertedBlueClusterIcons',
+    
+    openBalloonOnClick: false,  // Added this line
+
 });
 
+   
 
 
     var geoObjects = [];
     var $ = jQuery.noConflict();
-    var username = '';
-    var password = '';
+    var username = 'admin';
+    var password = '9x#?XgjE1@@W';
 
 
 
@@ -76,62 +77,56 @@ ymaps.ready(function () {
     newsList.style.display = 'none';
     document.body.appendChild(newsList);
 
-    var fullNewsModal = document.createElement('div');
-    fullNewsModal.id = 'full-news-modal';
-    fullNewsModal.style.position = 'fixed';
-    fullNewsModal.style.top = '50%';
-    fullNewsModal.style.left = '50%';
-    fullNewsModal.style.transform = 'translate(-50%, -50%)';
-    fullNewsModal.style.background = '#fff';
-    fullNewsModal.style.padding = '1em';
-    fullNewsModal.style.width = '60%';
-    fullNewsModal.style.height = '95%';
-    fullNewsModal.style.overflowY = 'scroll';
-    fullNewsModal.style.display = 'none';
-    document.body.appendChild(fullNewsModal);
 
+var fullNewsModal = document.createElement('div');
+fullNewsModal.id = 'full-news-modal';
+fullNewsModal.style.position = 'fixed';
+fullNewsModal.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+fullNewsModal.style.top = '50%';
+fullNewsModal.style.left = '50%';
+fullNewsModal.style.transform = 'translate(-50%, -50%)';
+fullNewsModal.style.background = '#fff';
+fullNewsModal.style.padding = '1em';
+fullNewsModal.style.width = '45%'; 
+fullNewsModal.style.height = '85%'; 
+fullNewsModal.style.display = 'none';
+fullNewsModal.style.borderRadius = '10px'; 
+document.body.appendChild(fullNewsModal);
+
+var modalContent = document.createElement('div');  // создаем контейнер для контента модального окна
+modalContent.style.position = 'relative';
+modalContent.style.width = '100%';
+modalContent.style.height = '100%';
+fullNewsModal.appendChild(modalContent);
     $.ajax({
         url: 'https://api.in-map.ru/api/news/',
         method: 'GET',
         beforeSend: function (xhr) {
             xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
         },
+        
         success: function (data) {
             data.forEach(function (news, index) {
                 ymaps.geocode(news.address)
                     .then(function (res) {
                         var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
-                        var pointOptions;
-
+                        
                         // Цветовая маркировка
                         switch (news.priority) { 
                             case 1:
-                                pointOptions = { preset: 'islands#greenIcon' };
+                                pointOptions = { preset: 'islands#greenCircleDotIcon' };
                                 break;
                             case 2:
-                                pointOptions = { preset: 'islands#yellowIcon' };
+                                pointOptions = { preset: 'islands#yellowCircleDotIcon' };
                                 break;
                             case 3:
-                                pointOptions = { preset: 'islands#redIcon' };
+                                pointOptions = { preset: 'islands#redCircleDotIcon' };
                                 break;
                             default:
-                                pointOptions = { preset: 'islands#violetIcon' };
+                                pointOptions = { preset: 'islands#violetCircleDotIcon' };
                                 break;
                         }
-
-                        var geoObject = new ymaps.Placemark(coordinates, pointOptions);
-                        geoObjects.push(geoObject);
-                        clusterer.add(geoObject);
-                    });
-            });
-        },
-        success: function (data) {
-            data.forEach(function (news, index) {
-                ymaps.geocode(news.address)
-                    .then(function (res) {
-                        var coordinates = res.geoObjects.get(0).geometry.getCoordinates();
-                        var pointOptions = { preset: 'islands#violetIcon' };
-                        var geoObject = new ymaps.Placemark(coordinates, pointOptions);
+                        var geoObject = new ymaps.Placemark(coordinates, null, pointOptions);
                         geoObject.properties.set('newsData', news);
                         geoObjects.push(geoObject);
                         clusterer.add(geoObject);
@@ -147,8 +142,7 @@ ymaps.ready(function () {
 
 
     });
-   
-      
+
     
     clusterer.events.add('click', function (e) {
         var target = e.get('target');
@@ -170,18 +164,33 @@ ymaps.ready(function () {
 
        
 
-    window.showFullNews = function(index) {
-        var geoObject = geoObjects[index];
-        var newsData = geoObject.properties.get('newsData');
-        var fullNewsContent = '<button onclick="hideFullNews()">Закрыть</button>'+'<h4>' + newsData.title + '</h4>' +
+   window.showFullNews = function(index) {
+    var geoObject = geoObjects[index];
+    var newsData = geoObject.properties.get('newsData');
+    var fullNewsContent = '<div style="display: flex; justify-content: space-between; align-items: center;">' + 
+                            '<p></p>' + 
+                            '<img src="data:image/svg+xml;utf8,<svg xmlns=\%22http://www.w3.org/2000/svg\%22 width=\%2224\%22 height=\%2224\%22 viewBox=\%220 0 24 24\%22 fill=\%22none\%22 stroke=\%22%23000000\%22 stroke-width=\%222\%22 stroke-linecap=\%22round\%22 stroke-linejoin=\%22round\%22><line x1=\%2218\%22 y1=\%226\%22 x2=\%226\%22 y2=\%2218\%22/><line x1=\%226\%22 y1=\%226\%22 x2=\%2218\%22 y2=\%2218\%22/></svg>" style="cursor: pointer;" onclick="hideFullNews()">' +
+                          '</div>' + '<h4>' + newsData.title + '</h4>'+ '<div style="padding-top: 20px;">' +
+                          '<img src="data:image/jpeg;base64,' + newsData.img + 
+                          '" style="max-width: 90%; max-height: 90%; display: block; margin: auto;">' +
+                          '<div style="padding-top: 20px;">' + 
+                               
                               '<p>' + newsData.description + '</p>' +
-                              '<img src="data:image/jpeg;base64,' + newsData.img + '" style="width: 90%; height: 90%;">' 
-                            ;
-        fullNewsModal.innerHTML = fullNewsContent;
-        fullNewsModal.style.display = 'block';
-    };
+                          '</div>'; 
+    fullNewsModal.innerHTML = fullNewsContent;
+    fullNewsModal.style.display = 'block';
+    
+};
 
-    window.hideFullNews = function() {
-        fullNewsModal.style.display = 'none';
-    };
+window.hideFullNews = function() {
+    fullNewsModal.style.display = 'none';
+    
+};
+
+window.onclick = function(event) {
+  if (event.target == fullNewsModal && event.target != modalContent) {
+    fullNewsModal.style.display = 'none';
+  }
+
+};
 });
