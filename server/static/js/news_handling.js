a69d26835f9d0bdec82a499a8541c4d7e9bbdb6b
codlxflare <148866504+codlxflare@users.noreply.github.com>
2024-06-10 18:17:32 +0300

    Update news_handling.js

--- server/static/js/news_handling.js
+++ server/static/js/news_handling.js
@@ -140,21 +140,27 @@ var newsDate = new Date(newsData.news_date);
 var formattedNewsDate = newsDate.toLocaleString('ru-RU', {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'});
 
 var fullNewsContent = '<div style="display: flex; justify-content: space-between; align-items: center;">' + 
-                            '<p></p>' + 
-                            '<img src="data:image/svg+xml;utf8,<svg xmlns=\%22http://www.w3.org/2000/svg\%22 width=\%2224\%22 height=\%2224\%22 viewBox=\%220 0 24 24\%22 fill=\%22none\%22 stroke=\%22%23000000\%22 stroke-width=\%222\%22 stroke-linecap=\%22round\%22 stroke-linejoin=\%22round\%22><line x1=\%2218\%22 y1=\%226\%22 x2=\%226\%22 y2=\%2218\%22/><line x1=\%226\%22 y1=\%226\%22 x2=\%2218\%22 y2=\%2218\%22/></svg>" style="cursor: pointer;" onclick="hideFullNews()">' +
-                          '</div>' + '<h4>' + newsData.title + '</h4>'+ 
-                          '<p style="color: #888;"><img src="data:image/svg+xml;charset=utf-8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'18\' height=\'18\' viewBox=\'0 0 24 24\' fill=\'none\' stroke=\'%23000\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-linejoin=\'round\' class=\'feather feather-map-pin\'%3E%3Cpath d=\'M21 10c0 7.5-9 13-9 13S3 17.5 3 10a9 9 0 1118 0z\'%3E%3C/path%3E%3Ccircle cx=\'12\' cy=\'10\' r=\'3\'%3E%3C/circle%3E%3C/svg%3E" style="margin-right: 5px;">' + newsData.address + '</p>' + 
-                          '<div style="position: absolute; bottom: 10px; right: 10px; color: #888;">' + formattedNewsDate + '</div>' +
-                          '<div style="padding-top: 20px;">' +
-                          '<img src="data:image/jpeg;base64,' + newsData.img + 
-                          '" style="max-width: 90%; max-height: 90%; display: block; margin: auto;">' +
-                          '<div style="padding-top: 20px;">' + 
-                               
-                              '<p>' + newsData.description + '</p>' +'<div style="padding-top: 20px; text-align: left;">' + 
-                              '<a href="' + newsData.url + '" target="_blank" style="color: #007bff; text-decoration: none;">Перейти к источнику</a>' +
-                          '</div>'; 
-fullNewsModal.innerHTML = fullNewsContent;
-fullNewsModal.style.display = 'block';
+                                '<p></p>' + 
+                                '<img src="data:image/svg+xml;utf8,<svg xmlns=\%22http://www.w3.org/2000/svg\%22 width=\%2224\%22 height=\%2224\%22 viewBox=\%220 0 24 24\%22 fill=\%22none\%22 stroke=\%22%23000000\%22 stroke-width=\%222\%22 stroke-linecap=\%22round\%22 stroke-linejoin=\%22round\%22><line x1=\%2218\%22 y1=\%226\%22 x2=\%226\%22 y2=\%2218\%22/><line x1=\%226\%22 y1=\%226\%22 x2=\%2218\%22 y2=\%2218\%22/></svg>" style="cursor: pointer;" onclick="hideFullNews()">' +
+                              '</div>' + '<h4>' + newsData.title + '</h4>'+ 
+                              '<p style="color: #888;"><img src="data:image/svg+xml;charset=utf-8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'18\' height=\'18\' viewBox=\'0 0 24 24\' fill=\'none\' stroke=\'%23000\' stroke-width=\'2\' stroke-linecap=\'round\' stroke-linejoin=\'round\' class=\'feather feather-map-pin\'%3E%3Cpath d=\'M21 10c0 7.5-9 13-9 13S3 17.5 3 10a9 9 0 1118 0z\'%3E%3C/path%3E%3Ccircle cx=\'12\' cy=\'10\' r=\'3\'%3E%3C/circle%3E%3C/svg%3E" style="margin-right: 5px;">' + newsData.address + '</p>' + 
+                              '<div style="position: absolute; bottom: 10px; right: 10px; color: #888;">' + formattedNewsDate + '</div>' +
+                              '<div style="padding-top: 20px;">';
+    
+    if (newsData.img!="a") {
+        fullNewsContent += '<img id="news-image" src="data:image/jpeg;base64,' + newsData.img + 
+                              '" style="max-width: 80%; max-height: 80%; display: block; margin: auto;">' + 
+                              '<div style="padding-top: 20px;">';
+    }
+
+    fullNewsContent += '<p>' + newsData.description + '</p>' +
+                       '<div style="padding-top: 20px; text-align: left;">' + 
+                       '<a href="' + newsData.url + '" target="_blank" style="color: #007bff; text-decoration: none;">Перейти к источнику</a>' +
+                       '</div>';
+    fullNewsModal.querySelector('.modal-content').innerHTML = fullNewsContent;
+    fullNewsModal.style.display = 'block';
+
+    
     
 };
 
