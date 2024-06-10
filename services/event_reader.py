5e94d5511bc0b131b41e867adacc4842b3d4f814
Vladimir Bezrukov <82569672+Protogenic@users.noreply.github.com>
2024-06-10 13:34:44 +0300

    Update event_reader.py

--- services/event_reader.py
+++ services/event_reader.py
@@ -22,21 +22,18 @@ async def kassir(logger):
     site_url = 'https://nn.kassir.ru/selection/luchshee-v-nijnem-novgorode'
     data = check_response(site_url)
     if not (data is None or prev_data == data):
-        index = 1
         for event in data.find_all('li', class_='my-4.5 xl:my-4 lg:my-2 md:my-5 sm:my-3 xs:my-2')[:event_limit]:
-            print(index)
-            index = index + 1
             try:
                 url = 'https://nn.kassir.ru' + event.find('a', {
                     'class': 'recommendation-item_img-block compilation-tile__img-block'}).get('href')
                 img = get_after_find(event, 'img', 'src')
                 img = base64.b64encode(requests.get(img).content).decode('utf-8')
                 title = event.find('h2').get_text()
-                address = event.find('a', {
-                    'class': 'recommendation-item_venue compilation-tile__venue hover:underline'}).get_text()
-                address = "Нижний Новгород," + address
                 date = get_after_find(event, 'time', 'datetime')
                 date = parser.parse(date).strftime("%Y-%m-%d %H:%M")
+
+                data = check_response(url)
+                address = data.find('p', class_='mb-2').get_text()
                 await send_json(title, date, address, url, img, logger)
             except AttributeError as e:
                 print("Error occurred while parsing event")
